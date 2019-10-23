388154c6a756a81affd46f86a401e0f0237c08e6
Alice Hutley <alice.hutley@gmail.com>
2019-10-23 15:23:33 +1100

    adding dragon species 2, 3, and 4

--- animal-reference.js
+++ animal-reference.js
@@ -1,9 +1,51 @@
 //Dragons
 //Chinese species
 var dragonSpecies1 = {
-"species" : "Chinese",
-"legs" : 4,
-"wings" : 0,
-"feathers" : ["Mane", "End of tail"],
-"colours" : ["green", "blue"],
+  "species" : "Chinese",
+  "legs" : 4,
+  "wings" : 0,
+  "feathers" : [
+    "Mane",
+    "End of tail",
+  ]
+  "colours" : [
+      "green",
+      "blue"
+    ],
 };
+var dragonSpecies2 = {
+  "species" : "Class 4",
+  "legs" : 2,
+  "wings" : 2,
+  "feathers" : 0,
+  "colours" : [
+    "green",
+    "yellow",
+    "grey",
+  ],
+}
+var dragonSpecies3 = {
+    "species" : "Class 6",
+    "legs" : 4,
+    "wings" : 2,
+    "feathers" : 0,
+    "colours" [
+        "green",
+        "blue",
+        "black",
+    ],
+}
+var dragonSpecies4 = {
+    "species" : "Class 8",
+    "legs" : 4,
+    "wings" : 4,
+    "feathers" : [
+        "Ridge on tail",
+        "End of tail",
+    ],
+    "colours" : [
+        "blue",
+        "purple",
+        "green",
+    ],
+}
