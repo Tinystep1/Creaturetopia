959dd5dc2926c79d36901807e12c61d75bb1979f
Alice Hutley <55948506+Tinystep1@users.noreply.github.com>
2019-11-21 14:32:46 +1100

    Fixing accidental folder to fix GiHub pages site

---
+++ animal-reference.js
@@ -0,0 +1,54 @@
+//Dragons
+//Chinese species
+var dragonSpecies1 = {
+  "species" : "Chinese",
+  "legs" : 4,
+  "wings" : 0,
+  "feathers" : [
+    "Mane",
+    "End of tail",
+  ]
+  "colours" : [
+      "green",
+      "blue"
+    ],
+};
+//Class 4 species
+var dragonSpecies2 = {
+  "species" : "Class 4",
+  "legs" : 2,
+  "wings" : 2,
+  "feathers" : 0,
+  "colours" : [
+    "green",
+    "yellow",
+    "grey",
+  ],
+}
+//Class 6 species
+var dragonSpecies3 = {
+    "species" : "Class 6",
+    "legs" : 4,
+    "wings" : 2,
+    "feathers" : 0,
+    "colours" [
+        "green",
+        "blue",
+        "black",
+    ],
+}
+//Class 8 species
+var dragonSpecies4 = {
+    "species" : "Class 8",
+    "legs" : 4,
+    "wings" : 4,
+    "feathers" : [
+        "Ridge on tail",
+        "End of tail",
+    ],
+    "colours" : [
+        "blue",
+        "purple",
+        "green",
+    ],
+}
